bea92b68b1fe43ffe694c3f4fda683116dee2b63
chuyentt <chuyentt@gmail.com>
2015-04-30 11:39:51 +0700

    Updated Install / Remove script

--- administrator/sql/uninstall.mysql.utf8.sql
+++ administrator/sql/uninstall.mysql.utf8.sql
@@ -1,1 +1,3 @@
 DROP TABLE IF EXISTS `#__epsg_projection`;
+DELETE FROM `#__ucm_history` WHERE `ucm_type_id` IN (SELECT `type_id` FROM `#__content_types` WHERE `type_alias` ='com_epsg.projection');
+DELETE FROM `#__content_types` WHERE `type_alias` = 'com_epsg.projection';
